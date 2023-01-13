d01712fe63532168b860aaef0b9ca09d56f9f252
farsam10 <90097506+farsam10@users.noreply.github.com>
2023-01-13 20:10:49 +0400

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,6 +10,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var myPageController = PageController(viewportFraction: 0.9, initialPage: 0);
+  int currentIndex = 0;
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scrollMyBanners();
+  }
+
+  void scrollMyBanners() {
+    Timer.periodic(
+      Duration(seconds: 2),
+      (timer) {
+        print("Time Completed");
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,20 +67,25 @@ class _HomeState extends State<Home> {
               ),
             ),
             SizedBox(height: 30),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+            Container(
+              height: MediaQuery.of(context).size.height / 4,
+              width: MediaQuery.of(context).size.width,
+              child: PageView(
+                controller: myPageController,
+                scrollDirection: Axis.horizontal,
                 children: [
                   ...List.generate(
                     3,
-                    (index) => Container(
-                      height: MediaQuery.of(context).size.height / 4,
-                      width: MediaQuery.of(context).size.width - 80,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 242, 143, 143),
-                        borderRadius: BorderRadius.circular(15),
+                    (index) => Padding(
+                      padding: const EdgeInsets.only(right: 10, left: 10),
+                      child: Container(
+                        // width: MediaQuery.of(context).size.width - 100,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 242, 143, 143),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        // margin: EdgeInsets.only(left: 10, right: 10),
                       ),
-                      margin: EdgeInsets.only(right: 11, left: 11),
                     ),
                   ),
                 ],
